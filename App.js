02d4a56e37bc331c6d7ce439a77ba56ba1f94da7
Badral <badral@mail.com>
2024-04-29 11:34:10 +0800

    last

--- App.js
+++ App.js
@@ -13,40 +13,46 @@ import "./src/provider/Provider";
 import { ProviderContext } from "./src/provider/Provider";
 
 LogBox.ignoreLogs([
-  "Sending `onAnimatedValueUpdate` with no listeners registered.",
-  "Non-serializable values were found in the navigation state",
+    "Sending `onAnimatedValueUpdate` with no listeners registered.",
+    "Non-serializable values were found in the navigation state",
 ]);
 
 const App = () => {
-  const provider = useContext(ProviderContext);
-  return (
-    <SheetProvider>
-      <ToastProvider
-        animationDuration={250}
-        animationType="slide-in"
-        duration={1000}
-        successColor="#54AE93"
-        successIcon={
-          <Icon name="information-circle-outline" size={20} color="#fff" />
-        }
-        textStyle={{ fontSize: 13, color: "#fff" }}
-      >
-        <SafeAreaProvider style={{ flex: 1, backgroundColor: "#F2F2F2" }}>
-          <SafeAreaView
-            style={{ flex: 1, backgroundColor: "white" }}
-            edges={["top"]}
-            mode="margin"
-          >
-            <NavigationContainer>
-              <StatusBar barStyle="dark-content" />
-              <StackNavigators />
-              {provider.calendarVisible && <CustomCalendar />}
-            </NavigationContainer>
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </ToastProvider>
-    </SheetProvider>
-  );
+    const provider = useContext(ProviderContext);
+    return (
+        <SheetProvider>
+            <ToastProvider
+                animationDuration={250}
+                animationType="slide-in"
+                duration={1000}
+                successColor="#54AE93"
+                successIcon={
+                    <Icon
+                        name="information-circle-outline"
+                        size={20}
+                        color="#fff"
+                    />
+                }
+                textStyle={{ fontSize: 13, color: "#fff" }}
+            >
+                <SafeAreaProvider
+                    style={{ flex: 1, backgroundColor: "#F2F2F2" }}
+                >
+                    <SafeAreaView
+                        style={{ flex: 1, backgroundColor: "white" }}
+                        edges={["top"]}
+                        mode="margin"
+                    >
+                        <NavigationContainer>
+                            <StatusBar barStyle="dark-content" />
+                            <StackNavigators />
+                            {provider.calendarVisible && <CustomCalendar />}
+                        </NavigationContainer>
+                    </SafeAreaView>
+                </SafeAreaProvider>
+            </ToastProvider>
+        </SheetProvider>
+    );
 };
 
 export default App;
--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -34,156 +34,207 @@ import UserHomeScreen from "../screens/user/UserHomeScreen";
 const Stack = createNativeStackNavigator();
 
 const StackNavigators = ({}) => {
-  const provider = useContext(ProviderContext);
+    const provider = useContext(ProviderContext);
 
-  return (
-    <Stack.Navigator initialRouteName="LoginScreen">
-      {!provider.isLoggedIn ? (
-        <Stack.Group>
-          <Stack.Screen
-            name="LoginScreen"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="RegisterScreen"
-            component={RegisterScreen}
-            options={{
-              headerShown: false,
-              animation: "slide_from_left",
-            }}
-          />
-          <Stack.Screen
-            name="OtpScreen"
-            component={OtpScreen}
-            options={{
-              headerShown: false,
-              animation: "slide_from_left",
-            }}
-          />
-          <Stack.Screen
-            name="ResetPasswordScreen"
-            component={ResetPasswordScreen}
-            options={{
-              headerShown: false,
-              animation: "slide_from_left",
-            }}
-          />
-          <Stack.Screen
-            name="ForgotPasswordScreen"
-            component={ForgotPasswordScreen}
-            options={{
-              headerShown: false,
-              animation: "slide_from_left",
-            }}
-          />
-          <Stack.Screen
-            name="TestScreen"
-            component={UserHomeScreen}
-            options={{
-              headerShown: false,
-              animation: "slide_from_left",
-            }}
-          />
-          {/* <Stack.Screen name="Screen" */}
-        </Stack.Group>
-      ) : (
-        <Stack.Group>
-          {provider.userData.role === "Manager" ? (
-            <Stack.Group screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="ManagerTab" component={TabNavigator} />
-              <Stack.Screen name="UserInfoScreen" component={UserInfoScreen} />
-              <Stack.Screen name="AddWorkScreen" component={AddWorkScreen} />
-              <Stack.Screen name="JobScreen" component={JobScreen} />
-              <Stack.Screen
-                name="EditGroupScreen"
-                component={EditGroupScreen}
-              />
-              <Stack.Screen
-                name="TimesheetScreen"
-                component={ManagerTimeSheet}
-              />
-              <Stack.Screen
-                name="SendNotification"
-                component={SendNotification}
-              />
-              <Stack.Screen
-                name="ProjectLocationScreen"
-                component={ProjectLocation}
-              />
-              <Stack.Screen
-                name="WorkerInfoScreen"
-                component={WorkerInfoScreen}
-              />
-              <Stack.Screen
-                name="AddWorkerScreen"
-                component={AddWorkerScreen}
-              />
-              <Stack.Screen
-                name="UserProfileScreen"
-                component={UserProfileScreen}
-              />
-              <Stack.Screen
-                name="CreateProjectLocationScreen"
-                component={CreateProjectLocation}
-              />
-            </Stack.Group>
-          ) : provider.userData.role === "Leader" ? (
-            <Stack.Group screenOptions={{ headerShown: false }}>
-              <Stack.Screen
-                name="UserMainScreen"
-                component={UserTabNavigator}
-              />
-              <Stack.Screen
-                name="LeaderInfoScreen"
-                component={UserInfoScreen}
-              />
-              <Stack.Screen
-                name="EditGroupScreen"
-                component={EditGroupScreen}
-              />
-              <Stack.Screen name="JobScreen" component={UserJobScreen} />
-              <Stack.Screen name="TimesheetScreen" component={UserTimeSheet} />
-              <Stack.Screen name="UserInfoScreen" component={UserInfoScreen} />
-              <Stack.Screen name="BankInfoScreen" component={BankInfoScreen} />
-              <Stack.Screen name="WorkInfoScreen" component={WorkInfoScreen} />
-              <Stack.Screen
-                name="WorkerInfoScreen"
-                component={WorkerInfoScreen}
-              />
-            </Stack.Group>
-          ) : (
-            <Stack.Group screenOptions={{ headerShown: false }}>
-              <Stack.Screen
-                name="UserMainScreen"
-                component={UserTabNavigator}
-              />
-              <Stack.Screen
-                name="EditGroupScreen"
-                component={EditGroupScreen}
-              />
-              <Stack.Screen name="JobScreen" component={UserJobScreen} />
-              <Stack.Screen name="TimesheetScreen" component={UserTimeSheet} />
-              <Stack.Screen name="UserInfoScreen" component={UserInfoScreen} />
-              <Stack.Screen name="BankInfoScreen" component={BankInfoScreen} />
-              <Stack.Screen name="WorkInfoScreen" component={WorkInfoScreen} />
-              <Stack.Screen
-                name="WorkerInfoScreen"
-                component={WorkerInfoScreen}
-              />
-            </Stack.Group>
-          )}
-          <Stack.Group screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="WorkerScreen" component={WorkerScreen} />
-            <Stack.Screen name="ChatScreen" component={ChatScreen} />
-            <Stack.Screen name="ChatUsers" component={ChatUsers} />
-            <Stack.Screen name="FilesScreen" component={FilesScreen} />
-            <Stack.Screen name="FileInfo" component={FileScreen} />
-          </Stack.Group>
-        </Stack.Group>
-      )}
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator initialRouteName="LoginScreen">
+            {!provider.isLoggedIn ? (
+                <Stack.Group>
+                    <Stack.Screen
+                        name="LoginScreen"
+                        component={LoginScreen}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="RegisterScreen"
+                        component={RegisterScreen}
+                        options={{
+                            headerShown: false,
+                            animation: "slide_from_left",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="OtpScreen"
+                        component={OtpScreen}
+                        options={{
+                            headerShown: false,
+                            animation: "slide_from_left",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="ResetPasswordScreen"
+                        component={ResetPasswordScreen}
+                        options={{
+                            headerShown: false,
+                            animation: "slide_from_left",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="ForgotPasswordScreen"
+                        component={ForgotPasswordScreen}
+                        options={{
+                            headerShown: false,
+                            animation: "slide_from_left",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="TestScreen"
+                        component={UserHomeScreen}
+                        options={{
+                            headerShown: false,
+                            animation: "slide_from_left",
+                        }}
+                    />
+                    {/* <Stack.Screen name="Screen" */}
+                </Stack.Group>
+            ) : (
+                <Stack.Group>
+                    {provider.userData.role === "Manager" ? (
+                        <Stack.Group screenOptions={{ headerShown: false }}>
+                            <Stack.Screen
+                                name="ManagerTab"
+                                component={TabNavigator}
+                            />
+                            <Stack.Screen
+                                name="UserInfoScreen"
+                                component={UserInfoScreen}
+                            />
+                            <Stack.Screen
+                                name="AddWorkScreen"
+                                component={AddWorkScreen}
+                            />
+                            <Stack.Screen
+                                name="JobScreen"
+                                component={JobScreen}
+                            />
+                            <Stack.Screen
+                                name="EditGroupScreen"
+                                component={EditGroupScreen}
+                            />
+                            <Stack.Screen
+                                name="TimesheetScreen"
+                                component={ManagerTimeSheet}
+                            />
+                            <Stack.Screen
+                                name="SendNotification"
+                                component={SendNotification}
+                            />
+                            <Stack.Screen
+                                name="ProjectLocationScreen"
+                                component={ProjectLocation}
+                            />
+                            <Stack.Screen
+                                name="WorkerInfoScreen"
+                                component={WorkerInfoScreen}
+                            />
+                            <Stack.Screen
+                                name="AddWorkerScreen"
+                                component={AddWorkerScreen}
+                            />
+                            <Stack.Screen
+                                name="UserProfileScreen"
+                                component={UserProfileScreen}
+                            />
+                            <Stack.Screen
+                                name="CreateProjectLocationScreen"
+                                component={CreateProjectLocation}
+                            />
+                        </Stack.Group>
+                    ) : provider.userData.role === "Leader" ? (
+                        <Stack.Group screenOptions={{ headerShown: false }}>
+                            <Stack.Screen
+                                name="UserMainScreen"
+                                component={UserTabNavigator}
+                            />
+                            <Stack.Screen
+                                name="LeaderInfoScreen"
+                                component={UserInfoScreen}
+                            />
+                            <Stack.Screen
+                                name="EditGroupScreen"
+                                component={EditGroupScreen}
+                            />
+                            <Stack.Screen
+                                name="JobScreen"
+                                component={UserJobScreen}
+                            />
+                            <Stack.Screen
+                                name="TimesheetScreen"
+                                component={UserTimeSheet}
+                            />
+                            <Stack.Screen
+                                name="UserInfoScreen"
+                                component={UserInfoScreen}
+                            />
+                            <Stack.Screen
+                                name="BankInfoScreen"
+                                component={BankInfoScreen}
+                            />
+                            <Stack.Screen
+                                name="WorkInfoScreen"
+                                component={WorkInfoScreen}
+                            />
+                            <Stack.Screen
+                                name="WorkerInfoScreen"
+                                component={WorkerInfoScreen}
+                            />
+                        </Stack.Group>
+                    ) : (
+                        <Stack.Group screenOptions={{ headerShown: false }}>
+                            <Stack.Screen
+                                name="UserMainScreen"
+                                component={UserTabNavigator}
+                            />
+                            <Stack.Screen
+                                name="EditGroupScreen"
+                                component={EditGroupScreen}
+                            />
+                            <Stack.Screen
+                                name="JobScreen"
+                                component={UserJobScreen}
+                            />
+                            <Stack.Screen
+                                name="TimesheetScreen"
+                                component={UserTimeSheet}
+                            />
+                            <Stack.Screen
+                                name="UserInfoScreen"
+                                component={UserInfoScreen}
+                            />
+                            <Stack.Screen
+                                name="BankInfoScreen"
+                                component={BankInfoScreen}
+                            />
+                            <Stack.Screen
+                                name="WorkInfoScreen"
+                                component={WorkInfoScreen}
+                            />
+                            <Stack.Screen
+                                name="WorkerInfoScreen"
+                                component={WorkerInfoScreen}
+                            />
+                        </Stack.Group>
+                    )}
+                    <Stack.Group screenOptions={{ headerShown: false }}>
+                        <Stack.Screen
+                            name="WorkerScreen"
+                            component={WorkerScreen}
+                        />
+                        <Stack.Screen
+                            name="ChatScreen"
+                            component={ChatScreen}
+                        />
+                        <Stack.Screen name="ChatUsers" component={ChatUsers} />
+                        <Stack.Screen
+                            name="FilesScreen"
+                            component={FilesScreen}
+                        />
+                        <Stack.Screen name="FileInfo" component={FileScreen} />
+                    </Stack.Group>
+                </Stack.Group>
+            )}
+        </Stack.Navigator>
+    );
 };
 
 export default StackNavigators;
